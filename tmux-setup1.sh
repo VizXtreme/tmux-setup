5a6af92a63a848c084ba33480105cdd6fe2c4468
VizXtreme <sk06pandit@gmail.com>
2025-07-10 23:15:00 +0530

    rev1

--- tmux-setup1.sh
+++ tmux-setup1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# 
-
 echo "---"
 echo "[+] Starting setup process..."
 echo "---"
 
 # Update and upgrade Termux packages, then install necessary tools & automatically input 'N' for configuration prompts during upgrade.
 
+echo " "
 echo "[+] Updating Termux packages..."
+echo " "
 apt update && apt upgrade -o Dpkg::Options::="--force-confold" -y
 pkg install python clang git wget curl openssh figlet make tsu x11-repo fastfetch toilet -y || { echo "[-] Failed to update."; exit 1; }
 echo "[✓] Updated successfully."
@@ -17,12 +17,16 @@ sleep 2
 
 # Clone the Termux-zsh repository, navigate into it, and run the setup script with automated 'Y' input
 
+echo " "
 echo "[+] Cloning repository..."
+echo " "
 git clone https://github.com/VizXtreme/Termux-zsh.git || { echo "[-] Failed to clone repository."; exit 1; }
 
 sleep 2
 
+echo " "
 echo "[+] Starting Termux-zsh setup..."
+echo " "
 sleep 2
 cd Termux-zsh
 yes 1 | bash setup.sh
--- tmux-setup2.sh
+++ tmux-setup2.sh
@@ -1,57 +1,46 @@
 #!/bin/bash
-#set -e
-
-# Optional: ensure git is installed
-command -v git >/dev/null 2>&1 || {
-  echo "[+] Installing git..."
-  pkg install -y git
-}
-
-# GitHub repository URL
-REPO_URL="https://github.com/VizXtreme/tmux-setup.git"
-
-# Temporary directory for clone
-TMP_DIR=$(mktemp -d)
 
 echo "---"
 echo "[+] Starting setup process..."
 echo "---"
 
 sleep 2
-
-echo "[+] Cloning repository..."
-git clone "$REPO_URL" "$TMP_DIR" || { echo "[-] Failed to clone repository."; exit 1; }
-
-sleep 2
-
+echo " "
 echo "[+] Copying files to home directory..."
-cd "$TMP_DIR"
-cp -a "$TMP_DIR"/. ~/ || { echo "[-] Failed to copy tmux files."; exit 1; }
+echo " "
+cd ~/tmux-setup
+cp -r .termux fastfetch OneShot-C ~/ || { echo "[-] Failed to copy tmux files."; exit 1; }
 echo "[✓] Files copied successfully."
 
 sleep 2
 
+echo " "
 echo "[+] Installing OneShot dependencies..."
+echo " "
 pkg install -y root-repo
 pkg install -y wpa-supplicant pixiewps iw openssl || { echo "[-] Failed to install OneShot dependencies."; exit 1; }
 echo "[✓] OneShot dependencies installed."
 
 sleep 2
 
+echo " "
 echo "[+] Compiling OneShot..."
+echo " "
 
 sleep 2
 
-cd ~/OneShot-C && make && chmod +x oneshot || { echo "[-] Failed to compile OneShot-C."; exit 1; }
+cd $HOME/OneShot-C && make && chmod +x oneshot || { echo "[-] Failed to compile OneShot-C."; exit 1; }
 echo "[✓] Compiled OneShot successfully (ignore the warnings)."
 
 sleep 2
 
+echo " "
 echo "[+] Cleaning up..."
-rm -rf "$TMP_DIR"
+cd ~/ && rm -rf tmux-setup
 
 sleep 2
 
+echo " "
 echo "[✓] Done."
 
 sleep 1
